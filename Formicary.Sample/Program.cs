4d9ec93426357bf955bbf363b8e91a977853be98
goodgoodman <gjwnsan9420@gmail.com>
2019-05-19 11:21:22 +0900

    임시 커밋

---
+++ Formicary.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Formicary.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Formicary/Server.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Formicary
+{
+    public class Server
+    {
+        public class Options
+        {
+            
+        }
+        private readonly ILogger _logger;
+        private readonly Options _options;
+
+        public Server(ILogger<Server> logger, IOptions<Options> options)
+        {
+            _logger = logger;
+            _options = options.Value;
+        }
+    }
+}
